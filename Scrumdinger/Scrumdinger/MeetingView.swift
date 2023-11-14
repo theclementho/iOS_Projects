bb1013c81e1af33b9132f65aecae21bd2011b6ee
Clement <theclementho@users.noreply.github.com>
2023-11-13 23:38:11 -0800

    Updated GitIgnore for mac, made a test change

--- Scrumdinger/Scrumdinger/MeetingView.swift
+++ Scrumdinger/Scrumdinger/MeetingView.swift
@@ -13,7 +13,7 @@ struct MeetingView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, World!")
         }
         .padding()
     }
